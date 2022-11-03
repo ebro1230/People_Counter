7425efd8d0e22c3ff71df74f4c0a5108d6c6b26c
edb1230 <ebro1230@gmail.com>
2022-11-03 21:32:28 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let count = 0;
+let countEl = document.getElementById("count-el")
+function increment(){
+    count++;
+    countEl.innerText = count;
+}
+function save(){
+    console.log(count);
+}
